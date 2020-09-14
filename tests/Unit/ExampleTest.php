abf9dd4bbc3a9c09cbca2d93c84b543346e1c3f9
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2020-09-14 20:01:04 +0200

    Show beforeEach, All and AfterEach, All

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,18 +1,26 @@
 <?php
 
-namespace Tests\Unit;
+declare(strict_types=1);
 
-use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
-    }
-}
+beforeEach(function() {
+    echo 'beforeEach' . PHP_EOL;
+});
+
+beforeAll(function() {
+    echo 'beforeAll' . PHP_EOL;
+});
+
+afterEach(function() {
+    echo 'afterEach' . PHP_EOL;
+});
+
+afterAll(function() {
+    echo 'afterAll' . PHP_EOL;
+});
+
+test('Runs basic test', function() {
+    echo 'test' . PHP_EOL;
+    expect(true)->toBeTrue();
+});
+
